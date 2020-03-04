3cf3610b37c8822cf3177b60e3e77f8361767fcd
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-04 09:43:26 +0000

    Adding Win Condition

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 12;
 const width = 600;
@@ -143,12 +143,14 @@ const goal = Bodies.rectangle(
   height - unitLength / 2,
   unitLength * 0.7,
   unitLength * 0.7,
-  { isStatic: true }
+  { label: "goal", isStatic: true }
 );
 World.add(world, goal);
 
 //Ball
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength * 0.25);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength * 0.25, {
+  label: "ball"
+});
 World.add(world, ball);
 
 document.addEventListener("keydown", event => {
@@ -165,4 +167,17 @@ document.addEventListener("keydown", event => {
   if (event.keyCode === 65) {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+//Win Condition
+Events.on(engine, "collisionStart", event => {
+  event.pairs.forEach(collision => {
+    const labels = ["ball", "goal"];
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      console;
+    }
+  });
 });
